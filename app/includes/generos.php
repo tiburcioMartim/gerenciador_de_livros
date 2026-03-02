9a0a187289722117e0497517b2fd27d1d0548426
Martim Tiburcio <tiburciomartim@gmail.com>
2026-03-02 16:19:44 -0300

    ajustes(includes, css): Adicionado generos.php e feito arrumação no style.css

---
+++ app/includes/generos.php
@@ -0,0 +1,12 @@
+<select class="select-primario meu-select" name="genero" required>
+    <option value="" disabled selected>Selecione o gênero</option>
+    <option value="fantasia">Fantasia</option>
+    <option value="comedia">Comédia</option>
+    <option value="drama">Drama</option>
+    <option value="infantil">Infantil</option>
+    <option value="romance">Romance</option>
+    <option value="aventura">Aventura</option>
+    <option value="ficcao">Ficção</option>
+    <option value="luta">Luta</option>
+    <option value="terror">Terror</option>
+</select>
--- app/pages/registrar-livro.php
+++ app/pages/registrar-livro.php
@@ -29,18 +29,7 @@ require_once __DIR__ . '/../includes/functions.php';
                 <input type="date" name="ano_publicacao" placeholder="Quando que foi publicado?" required>
 
                 <label for="genero">Gênero: </label>
-                <select class="select-primario" name="genero" required>
-                    <option value="" disabled selected>Selecione o gênero</option>
-                    <option value="fantasia">Fantasia</option>
-                    <option value="comedia">Comédia</option>
-                    <option value="drama">Drama</option>
-                    <option value="infantil">Infantil</option>
-                    <option value="romance">Romance</option>
-                    <option value="aventura">Aventura</option>
-                    <option value="ficcao">Ficção</option>
-                    <option value="luta">Luta</option>
-                    <option value="terror">Terror</option>
-                </select>
+                <?php  require_once __DIR__ . '/../includes/generos.php'; ?>   
 
                 <input class="btn-secundario" type="submit" name="registrar_livro" value="Registrar">
             </form>
